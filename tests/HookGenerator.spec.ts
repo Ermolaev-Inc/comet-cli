4069bfdc35a7414451912ab8809152640549ab18
Ilya <Ermolaev.ID@yandex.ru>
2021-08-20 19:00:19 +0500

    t(HookGenerator): create

---
+++ tests/HookGenerator.spec.ts
@@ -0,0 +1,57 @@
+import fs from "fs";
+import { HookGenerator } from "../src/generators/HookGenerator";
+import { HookPath } from "../src/paths/HookPath";
+import { Generator } from "../src/generators/Generator";
+
+const hookTemplate = `export const useStyles = () => {
+  return;
+};
+`;
+
+describe("HookGenerator tests", () => {
+  let hookGenerator: Generator;
+
+  beforeAll(() => {
+    class MockPath implements HookPath {
+      hookFile = (name: string) => `${this.hookFolder()}/${name}.hook.ts`;
+      hookFolder = () => `${process.cwd()}/playground/hooks`;
+    }
+    hookGenerator = new HookGenerator(new MockPath());
+  });
+
+  test("standard hook generating", async () => {
+    try {
+      await hookGenerator.generate("http");
+      await fs.promises.access(
+        `${process.cwd()}/playground/hooks/http.hook.ts`,
+      );
+      expect(true).toBe(true);
+    } catch (e) {
+      expect(false).toBe(true);
+    }
+  });
+
+  test("repeat standard hook generating", async () => {
+    try {
+      await hookGenerator.generate("http");
+      await fs.promises.access(
+        `${process.cwd()}/playground/hooks/http.hook.ts`,
+      );
+      expect(true).toBe(true);
+    } catch (e) {
+      expect(false).toBe(true);
+    }
+  });
+
+  test("hook template", async () => {
+    try {
+      await hookGenerator.generate("styles");
+      const content = await fs.promises.readFile(
+        `${process.cwd()}/playground/hooks/styles.hook.ts`,
+      );
+      expect(content.toString()).toBe(hookTemplate);
+    } catch (e) {
+      expect(false).toBe(true);
+    }
+  });
+});
